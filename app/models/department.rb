9d1bad85d7069a0a99469bd060b090483f860ac7
Haaris Memon <haaris.memon@kcl.ac.uk>
2017-02-08 18:38:46 +0000

    Generate a Department model

---
+++ app/models/department.rb
@@ -0,0 +1,5 @@
+class Department < ApplicationRecord
+
+  validates :name, presence: true, :uniqueness => true
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170208143124) do
+ActiveRecord::Schema.define(version: 20170208181753) do
+
+  create_table "departments", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "uni_modules", force: :cascade do |t|
     t.string   "name"
