3be8c598e0f9e40010211c59fe44154d40faff8b
k1502691 <vlad.nedelescu@kcl.ac.uk>
2017-02-08 22:57:13 +0000

    Add tag validations

--- db/schema.rb
+++ db/schema.rb
@@ -19,4 +19,11 @@ ActiveRecord::Schema.define(version: 20170208200909) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,7 +1,14 @@
-require 'rails_helper'
-
 FactoryGirl.define do
+  factory :tag do
+    name "Arbitrary Tag"
+    type "Career"
+  end
+
   factory :career do
     name "Software Engineer"
+  end
+
+  factory :interest do
+    name "Medicine"
   end
 end
