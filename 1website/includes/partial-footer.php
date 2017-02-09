179597f4c749b24d03efca6510df38b361c41b40
Feras Al-Hamadani <feras.al-hamadani@kcl.ac.uk>
2017-02-09 13:54:54 +0000

    Add header and footer to partial files

--- 1website/includes/partial-footer.php
+++ 1website/includes/partial-footer.php
@@ -7,7 +7,18 @@
 ?>
 <!--Footer stuff goes here-->
 <footer>
-   Khalil
+    <div class="footer-bottom">
+      <div class="container">
+        <img id="footer-logo" class="pull-left" src="assets/modulect-logo.png">
+        <div class="pull-right">
+          <ul class="fa-ul">
+            <a id="contact-us">Contact Us</a>
+  				  <i class="fa fa-twitter fa-2x" aria-hidden="true"></i>
+  					<i class="fa fa-youtube-play fa-2x" aria-hidden="true"></i>
+					</ul>
+        </div>
+      </div>
+    </div>
 </footer>
 <script>
    $(document).ready(function(){
