aa1ee8a3c3b9f03c84af8ced709553b4a259046e
Aqib Rashid <aqib.rashid@kcl.ac.uk>
2017-02-10 19:37:01 +0000

    Completely redesigned the results card, still got a weird collapse bug

--- 1website/results.php
+++ 1website/results.php
@@ -1,13 +1,13 @@
 <?php
-/**
-* Will form the login page / home page
-* author Aqib Rashid
-*/
-   $mock = "";
-   if(isset($_GET['mock'])) {
-     $mock = $_GET['mock'];
-   }
-   ?>
+   /**
+   * Will form the login page / home page
+   * author Aqib Rashid
+   */
+      $mock = "";
+      if(isset($_GET['mock'])) {
+        $mock = $_GET['mock'];
+      }
+      ?>
 <!doctype html>
 <html>
    <head>
@@ -15,27 +15,24 @@
       <?php include 'includes/assets.php'; ?>
    </head>
    <body>
-   <?php include 'includes/partial-header.php'; ?>
-
-   <!--VERY MESSY NOT GOOD CODING PRACTICE-->
-   <style>
-   .banner {
-       height: 270px;
-       background: #2986BE;
-       background-image: url(//p6.zdassets.com/hc/theme_assets/138842/200037786//pattern_transparent.png);
-       background-size: 380px, contain;
-       padding-top: 121px;
-       padding-bottom: 35px;
-       color:#fff;
-   }
-   </style>
-   <div class="banner">
-     <div class="container">
-       <h1>Results</h1>
-     </div>
-   </div>
-
-
+      <?php include 'includes/partial-header.php'; ?>
+      <!--VERY MESSY NOT GOOD CODING PRACTICE-->
+      <style>
+         .banner {
+         height: 270px;
+         background: #2986BE;
+         background-image: url(//p6.zdassets.com/hc/theme_assets/138842/200037786//pattern_transparent.png);
+         background-size: 380px, contain;
+         padding-top: 121px;
+         padding-bottom: 35px;
+         color:#fff;
+         }
+      </style>
+      <div class="banner">
+         <div class="container">
+            <h1>Results</h1>
+         </div>
+      </div>
       <div id="page" style="padding-top:30px;">
          <div class="container">
             <?php if($mock == "error"){?>
@@ -56,111 +53,127 @@
                </div>
             </div>
             <?php } ?>
-
-<div class="row" style="margin-bottom:20px;">
-   <div class="col-md-6 col-md-offset-1">
-<h1>Here's what we found:</h1>
-<h5>Showing 2 modules</h5>
-</div>
-
-<div class="col-md-4 text-right" style="padding-top:40px;">
-<div class="dropdown">
-Sort by: 
-<select>
-  <option value="tags" selected>Tag matches</option>
-  <option value="coursework">Coursework percentage</option>
-  <option value="exam">Exam percentage</option>
-  <option value="pass">Pass rate</option>
-</select>
-</div>
-</div>
-
-</div>
-
-
-
+            <div class="row" style="margin-bottom:20px;">
+               <div class="col-md-6 col-md-offset-1">
+                  <h1>Here's what we found:</h1>
+                  <h5>Showing 2 modules</h5>
+               </div>
+               <div class="col-md-4 text-right" style="padding-top:40px;">
+                  <div class="dropdown">
+                     Sort by: 
+                     <select>
+                        <option value="tags" selected>Tag matches</option>
+                        <option value="coursework">Coursework percentage</option>
+                        <option value="exam">Exam percentage</option>
+                        <option value="pass">Pass rate</option>
+                     </select>
+                  </div>
+               </div>
+            </div>
             <div class="row">
-
-<style>
-  .result-card-main{border-right:1px dotted #dfe2e1;}
-.btn-small{    width: 150px;
-    font-size: 13px;}
-@media (max-width: 768px) {
-
-  .result-card-main{border-right:0px;border-bottom:1px dotted #dfe2e1;margin-bottom:20px;padding-bottom:20px;}
-
-}
+               <style>
+                  .result-card-side{border-left:1px dotted #dfe2e1;}
+                  .btn-small{    width: 150px;
+                  font-size: 13px;}
+                  @media (max-width: 768px) {
+                  .result-card-side{border-left:0px;border-top:1px dotted #dfe2e1;border-bottom:1px dotted #dfe2e1;padding-top:20px;padding-bottom:20px;margin-top:20px;}
+                  }
+               </style>
 
 
-</style>
+
                <div class="col-md-10 col-md-offset-1">
                   <div class="col-md-12 login-card result-card" style="
-                  border-left-width: 5px;
-                  border-radius: .25rem;border-left-color: #2ab27b;">
-
-<div class="col-md-9 result-card-main">
-                     <h2>5CCS2INS Internet Systems</h2>
-                     <h4>Second Year, Semester 1 - 15 Credits</h4>
-
-                     <h5 style="margin-top:20px;">Description</h5>
-                     <p>The aims of this module are: to provide an overall understanding of the communication model used on the Internet,
-to provide an in-depth understanding of the main underlying software components of the Internet to provide an overview of the main languages used on the Internet,
-to provide an understanding of security threats to Internet application and main techniques used to tackle them.</p>
-
-<div style="margin-top:20px">
-  <a href="#" class="btn btn-lg btn-small"><i class="fa fa-star" aria-hidden="true"></i>&nbsp;&nbsp;Save Module</a>
-<a style="cursor:pointer;float:right;margin-right:10px;margin-top:5px;" data-toggle="collapse" data-target="#result-1-more">More info <i class="fa fa-angle-down" aria-hidden="true"></i></a></div>
-
-
-<div id="result-1-more" class="collapse" style="padding-top:25px;">
-  <h5 style="margin-top:20px;">Potential careers aspects for this module</h5>
-  <p>Read, from, model, okay</p>
-
-                       <h5 style="margin-top:20px;">Requirements/Pre-requisites</h5>
-    <p>(none)</p>
-                       <h5 style="margin-top:20px;">Lecturers</h5>
-<p>Dr Steve Phelps and Dr Samhar Mahmood</p>
-                       <h5 style="margin-top:20px;">Assessment methods</h5>
-    <p>80% exam, 20% coursework</p>
-
-
-</div>
-
-
-</div>
-<style>
-.result-side-column .row{margin-left:0px;margin-right:0px;}
-</style>
-
-<div class="col-md-3 result-side-column">
+                     border-left-width: 5px;
+                     border-radius: .25rem;border-left-color: #2ab27b;">
 
 <div class="row">
-<h5><i class="fa fa-tag" aria-hidden="true"></i>&nbsp;&nbsp;Tag match </h5>
-<div style="color:#2ab27b;font-size:18px;">80%</div>
-<p>4 out of 5</p>
+                     <div class="col-md-9 result-card-main">
+                        <h2>5CCS2INS Internet Systems</h2>
+                        <h4>Second Year, Semester 1 - 15 Credits</h4>
+                        <div style="margin-top:30px">
+                           <a href="#" class="btn btn-lg btn-small"><i class="fa fa-star" aria-hidden="true"></i>&nbsp;&nbsp;Save Module</a>
+                           <a style="cursor:pointer;float:right;margin-right:10px;margin-top:5px;" data-toggle="collapse" data-target="#result-1-more">More info <i class="fa fa-angle-down" aria-hidden="true"></i></a>
+                        </div>
+                     </div>
+                     <style>
+                        .result-side-column .row{margin-left:0px;margin-right:0px;}
+                     </style>
+                     <div class="col-md-3 result-side-column result-card-side">
+                        <div class="row">
+                           <h5><i class="fa fa-tag" aria-hidden="true"></i>&nbsp;&nbsp;Tag match </h5>
+                           <div style="color:#2ab27b;font-size:18px;">80%</div>
+                           <p>4 out of 5</p>
+                        </div>
+                        <div class="row" style="margin-top:10px">
+                           <h5><i class="fa fa-tags" aria-hidden="true"></i>&nbsp;&nbsp;Matched tags</h5>
+                           <div style="font-size:18px;color:#64a3c8">HERE</div>
+                        </div>
+                     </div>
+                     
+                     </div>
+
+<div id="result-1-more" class="collapse" style="margin-left:-15px;margin-top:20px;">
+
+
+
+                       
+
+<div class="col-md-12" style="margin-bottom:15px;">
+ <h5>Description</h5>
+                        <p>The aims of this module are: to provide an overall understanding of the communication model used on the Internet,
+                           to provide an in-depth understanding of the main underlying software components of the Internet to provide an overview of the main languages used on the Internet,
+                           to provide an understanding of security threats to Internet application and main techniques used to tackle them.
+                        </p>
 </div>
 
-<div class="row" style="margin-top:10px">
-<h5><i class="fa fa-tags" aria-hidden="true"></i>&nbsp;&nbsp;Matched tags</h5>
-<div style="font-size:18px;color:#64a3c8">HERE</div>
+
+<div class="col-md-4" style="margin-bottom:15px;">
+<h5><i class="fa fa-briefcase" aria-hidden="true"></i>&nbsp;&nbsp;Careers for this module</h5>
+                        <p>Read, from, model, okay</p>
+</div>
+
+<div class="col-md-4" style="margin-bottom:15px;">
+ <h5><i class="fa fa-check-circle-o" aria-hidden="true"></i>&nbsp;&nbsp;Requirements/Pre-requisites</h5>
+                        <p>(none)</p>
+                      
+</div>
+
+<div class="col-md-4" style="margin-bottom:15px;">
+  <h5><i class="fa fa-users" aria-hidden="true"></i>&nbsp;&nbsp;Lecturer(s)</h5>
+                        <p>Dr Steve Phelps and Dr Samhar Mahmood</p>
+</div>
+
+<div class="col-md-4" style="margin-bottom:15px;">
+
+                        <h5><i class="fa fa-book" aria-hidden="true"></i>&nbsp;&nbsp;Assessment methods</h5>
+                        <p>80% exam, 20% coursework</p>
+</div>
+
+<div class="col-md-4" style="margin-bottom:15px;">
+ <h5><i class="fa fa-pencil-square-o" aria-hidden="true"></i>&nbsp;&nbsp;Exam : Coursework ratio</h5>
+                        <p>80:20</p>
 </div>
 
 
-<div class="row" style="margin-top:10px">
-<h5><i class="fa fa-file-text-o" aria-hidden="true"></i>&nbsp;&nbsp;Exam : Coursework ratio</h5>
-<div style="font-size:18px;">80:20</div>
+<div class="col-md-4" style="margin-bottom:15px;">
+ <h5><i class="fa fa-graduation-cap" aria-hidden="true"></i>&nbsp;&nbsp;Pass rate</h5>
+                        <p>65%</p>
 </div>
 
 
-<div class="row" style="margin-top:10px">
-<h5>Module pass rate</h5>
-<div style="font-size:18px;">74%</div>
-</div>
+            
 
 
-</div>
 
 
+
+
+
+
+
+
+                     </div>
 
 
 
@@ -172,16 +185,15 @@ to provide an understanding of security threats to Internet application and main
 
 
 
+
+
+
+
+
+
+
+
             </div>
-
-
-
-
-
-
-
-
-
             <div class="row" style="margin-top:40px;">
                <div class="col-md-12 text-center">
                   <p>Need help? <a href="#">Contact KCL Modulect support</a>
