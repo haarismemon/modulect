2984e6222cbe0e2db8b9b89e2f245667151b40c0
Haaris Memon <haaris.memon@kcl.ac.uk>
2017-02-10 14:09:16 +0000

    Update seed file with record creation for modules and tags for database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,40 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+prp = UniModule.create(name: "Programming Practice", code: "4ccs1pra")
+pra = UniModule.create(name: "Programming with Practical Applications", code: "4ccs1prp")
+fc1 = UniModule.create(name: "Foundations of Computing 1", code: "4ccs1fc1")
+cs1 = UniModule.create(name: "Computer Systems", code: "4ccs1cs1")
+iai = UniModule.create(name: "Artificial Intelligence", code: "4ccs1iai")
+dst = UniModule.create(name: "Data Structures", code: "4ccs1dst")
+dbs = UniModule.create(name: "Database Systems", code: "4ccs1dbs")
+ela = UniModule.create(name: "Elementary Logic With Applications", code: "4ccs1ela")
+ins = UniModule.create(name: "Internet Systems", code: "5ccs2ins")
+fc2 = UniModule.create(name: "Foundations of Computing 2", code: "5ccs2fc2")
+
+programming = InterestTag.create(name: "Programming")
+maths = InterestTag.create(name: "Maths")
+robotics = InterestTag.create(name: "Robotics")
+algorithms = InterestTag.create(name: "Algorithms")
+artificial_intelligence = InterestTag.create(name: "Artificial Intelligence")
+software_engineer = CareerTag.create(name: "Software Engineering")
+network_engineer = CareerTag.create(name: "Network Engineer")
+logic_engineer = CareerTag.create(name: "Logic Engineer")
+database_engineer = CareerTag.create(name: "Database Engineer")
+hardware_engineer = CareerTag.create(name: "Database Engineer")
+
+prp.tags << programming
+pra.tags << programming
+fc1.tags << maths
+fc2.tags << maths
+iai.tags << robotics
+dst.tags << algorithms
+fc2.tags << algorithms
+iai.tags << artificial_intelligence
+prp.tags << software_engineer
+pra.tags << software_engineer
+ins.tags << network_engineer
+ela.tags << logic_engineer
+dbs.tags << database_engineer
+cs1.tags << hardware_engineer
