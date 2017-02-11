608e641d85334b5f8f761313759c2ad0d34108aa
Haaris Memon <haaris.memon@kcl.ac.uk>
2017-02-11 15:22:12 +0000

    Add ability to search for module with either name or code, in addition to tag names

--- app/models/uni_module.rb
+++ app/models/uni_module.rb
@@ -28,19 +28,42 @@ class UniModule < ApplicationRecord
     # for each tag, search for the modules that contain the tag name
     tags_array.each do |tag_name|
       # store the tag object relating to the tag name
-      tag = Tag.find_by_name(tag_name)
-      if tag
+      match_tag = Tag.find_by_name(tag_name)
+      if match_tag
         search(tag_name).each do |uni_module|
           # if the module was previously matched with a tag, store the current tag to the module's matched tags
-          if results.key?(uni_module)
-            matched_tags_array = results[uni_module]
-            matched_tags_array << tag
-            results[uni_module] = matched_tags_array
-            # if this is first matched tag, then store the tag in an array
+          if results.key?(uni_module.name)
+            matched_tags_array = results[uni_module.name]
+            matched_tags_array << match_tag.name
+            results[uni_module.name] = matched_tags_array
+          # if this is first matched tag, then store the tag in an array
           else
-            results[uni_module] = [tag]
+            results[uni_module.name] = [match_tag.name]
           end
         end
+      # else if the tag is not found, try look for a matching module name or code
+      else
+        # find module by code
+        match_module = UniModule.find_by_code(tag_name)
+
+        # if module not found with code, then find with name
+        unless match_module
+          match_module = UniModule.find_by_name(tag_name)
+        end
+
+        # if module found with either code or name, then add module to array of matched tags
+        if match_module
+          # if the module was previously matched with a tag, store the current module to the module's matched tags
+          if results.key?(match_module.name)
+            matched_tags_array = results[match_module.name]
+            matched_tags_array << match_module.name
+            results[match_module.name] = matched_tags_array
+          # if this is first matched tag, then store the module in an array
+          else
+            results[match_module.name] = [match_module.name]
+          end
+        end
+
       end
     end
 
