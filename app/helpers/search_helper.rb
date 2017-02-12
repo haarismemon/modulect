1dbf7eab3f1e497044718abb61d75cee27574326
Aqib Rashid <aqib.rashid@kcl.ac.uk>
2017-02-12 22:32:28 +0000

    Add a direct search function to assist with tag match counts

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -9,6 +9,28 @@ module SearchHelper
 	 		input
 	 	end
 
-	 end
+	end
+
+	# returns percentage tag match on input
+	# written by Aqib
+	def percentage_tag(module_matched, total)
+		(module_matched / total) * 100
+	end
+
+	# returns percentage tag match on input
+	# written by Aqib
+	def count_tag(module_matched, total)
+		"#{module_matched} out of #{total}"
+	end
+
+	# returns true if searched directly for a module in search
+	def direct_search_check(module_code, module_matched_list, tags_matched_list)
+		if module_matched_list.include?(module_code) && 
+			tags_matched_list.include?(module_code) 
+			true
+		else 
+			false
+		end
+	end
 
 end
