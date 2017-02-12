5f4193ce08b51f1698cdfa18b35f7199225bf3df
Aqib Rashid <aqib.rashid@kcl.ac.uk>
2017-02-12 21:59:18 +0000

    Add helper to check if data was given in model

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -1,2 +1,14 @@
 module SearchHelper
+
+	# returns "No data" string if input is empty since no data was provided in model
+	# written by Aqib
+	def size_check(input)
+		if input.nil? || input.length == 0
+			"No data available"
+	 	else
+	 		input
+	 	end
+
+	 end
+
 end
