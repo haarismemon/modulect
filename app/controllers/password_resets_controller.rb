e608f4e88deeda0df3c8db2ade17c0b748089ce1
k1502691 <vlad.nedelescu@kcl.ac.uk>
2017-02-12 23:11:33 +0000

    Add integration tests for password reset

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -65,7 +65,9 @@ class PasswordResetsController < ApplicationController
 
   # Confirms a valid user
   def valid_user
-    unless (@user && @user.activated? && @user.authenticated?(:reset, params[:id]))
+    unless (@user &&
+            @user.activated? &&
+            @user.authenticated?(:reset, params[:id]))
       redirect_to root_url
     end
   end
--- spec/controllers/uni_modules_controller_spec.rb
+++ spec/controllers/uni_modules_controller_spec.rb
@@ -9,32 +9,10 @@ RSpec.describe UniModulesController, type: :controller do
     end
   end
 
-  describe "GET #show" do
-    it "returns http success" do
-      get :show
-      expect(response).to have_http_status(:success)
-    end
-  end
-
   describe "GET #new" do
     it "returns http success" do
       get :new
       expect(response).to have_http_status(:success)
     end
   end
-
-  describe "GET #edit" do
-    it "returns http success" do
-      get :edit
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET #delete" do
-    it "returns http success" do
-      get :delete
-      expect(response).to have_http_status(:success)
-    end
-  end
-
 end
