e7dcc8a683ca788c00a02461c1c9ca2d6ffd335e
Aleksandr Sutin <aleksandr.sutin@kcl.ac.uk>
2017-02-13 02:36:46 +0000

    change departments routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,5 @@
 Rails.application.routes.draw do
 
-
-  get 'departments/index'
-
-  get 'departments/show'
-
-  get 'departments/new'
-
-  get 'departments/edit'
-
-  get 'departments/delete'
-
   root 'static_pages#home'
   get '/about', to: 'static_pages#about'
 
@@ -25,6 +14,13 @@ Rails.application.routes.draw do
 
   # Uni Modules
   resources :uni_modules do
+    member do
+      get :delete
+    end
+  end
+
+  # Departments
+  resources :departments do
     member do
       get :delete
     end
