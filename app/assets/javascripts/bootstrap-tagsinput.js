db44af1cfc8f523ed2cd3a7cf193b6cb3fb6d5ea
Feras Al-Hamadani <feras.al-hamadani@kcl.ac.uk>
2017-02-13 14:05:48 +0000

    Modify Javascript

--- app/assets/javascripts/bootstrap-tagsinput.js
+++ app/assets/javascripts/bootstrap-tagsinput.js
@@ -45,7 +45,7 @@
     this.inputSize = Math.max(1, this.placeholderText.length);
 
     this.$container = $('<div class="bootstrap-tagsinput"></div>');
-    this.$input = $('<input type="text" size="56" placeholder="' + this.placeholderText + '"/>').appendTo(this.$container);
+    this.$input = $('<input type="text" id="search" size="56" placeholder="' + this.placeholderText + '"/>').appendTo(this.$container);
 
     this.$element.before(this.$container);
 
