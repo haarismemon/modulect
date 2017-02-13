550611773f9f66ab516606d49ad3dd3d6e2a0ad5
Feras Al-Hamadani <feras.al-hamadani@kcl.ac.uk>
2017-02-13 23:27:45 +0000

    Correct search bar styling

--- app/assets/javascripts/bootstrap-tagsinput.js
+++ app/assets/javascripts/bootstrap-tagsinput.js
@@ -42,7 +42,7 @@
     this.multiple = (this.isSelect && element.hasAttribute('multiple'));
     this.objectItems = options && options.itemValue;
     this.placeholderText = element.hasAttribute('placeholder') ? this.$element.attr('placeholder') : '';
-    this.inputSize = Math.max(1, this.placeholderText.length);
+    this.inputSize = Math.max(56, this.placeholderText.length);
 
     this.$container = $('<div class="bootstrap-tagsinput"></div>');
     this.$input = $('<input type="text" size="56" placeholder="' + this.placeholderText + '"/>').appendTo(this.$container);
