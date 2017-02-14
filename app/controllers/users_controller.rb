83fb896c1f14c9bbe3c644ec11aef481a04f1296
Amin mnasour <amin.mansour@kcl.ac.uk>
2017-02-14 00:12:49 +0000

    Add create_by_admin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,21 @@ class UsersController < ApplicationController
     end
   end
 
+  def create_by_admin
+    # Instantiate a new object using form parameters
+    @user = User.new(user_creation_params)
+    # Save the object
+    if @user.save
+      # If save succeeds, redirect to the index action
+      flash[:notice] = "You have successfully created #{@user.full_name} and it's privileges have been granted"
+      redirect_to(users_path)
+    else
+      # If save fails, redisplay the form so user can fix problems
+      render(:new)
+
+    end
+  end
+
   # Handle signup form submission.
   # User level is set to Student.
   def create
