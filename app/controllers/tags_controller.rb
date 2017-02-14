da3eb9bed352faeed9620e40b63ba2dc6c1948a4
Amin mnasour <amin.mansour@kcl.ac.uk>
2017-02-14 02:43:24 +0000

    Create create action in TagsController

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -4,4 +4,17 @@ class TagsController < ApplicationController
     @tag = Tag.new
   end
 
+  def create
+    @tag = Tag.new(tags_params)
+    # Save the object
+    if @tag.save
+      # If save succeeds, redirect to the index action
+      flash[:notice] = @tag.name+" was created successfully."
+      redirect_to(tags_path)
+    else
+      # If save fails, redisplay the form so user can fix problems
+      render('new')
+    end
+  end
+
 end
