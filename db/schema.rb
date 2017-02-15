60c9e034e58a9053f47cc69a2366d39e35c4c411
Haaris Memon <haaris.memon@kcl.ac.uk>
2017-02-15 17:43:50 +0000

    Add Faculty model and migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170213192947) do
+ActiveRecord::Schema.define(version: 20170215174045) do
 
   create_table "courses", force: :cascade do |t|
     t.string   "name"
@@ -26,6 +26,12 @@ ActiveRecord::Schema.define(version: 20170213192947) do
   end
 
   create_table "departments", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "faculties", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
