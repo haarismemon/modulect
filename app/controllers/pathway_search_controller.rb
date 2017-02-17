113d164c2fcc3cb9b736a25e234193d135e55f91
Aqib Rashid <aqib.rashid@kcl.ac.uk>
2017-02-17 19:25:08 +0000

    Set up a very crude version of pathway search

--- app/controllers/pathway_search_controller.rb
+++ app/controllers/pathway_search_controller.rb
@@ -4,9 +4,30 @@ class PathwaySearchController < ApplicationController
 	end
 
 	def choose
+		if params.has_key?(:year) && !params[:year].empty? && params.has_key?(:course) && !params[:course].empty? 
+	      @year_of_study = params[:year]
+	      @course = params[:course]
+	    else
+	     redirect_to "/"
+	    end
 	end
 
 	def view_results
+
+		if params.has_key?(:year) && !params[:year].empty? && params.has_key?(:course) && !params[:course].empty? && params.has_key?(:chosen_tags) && !params[:chosen_tags].empty?
+	      @year_of_study = params[:year]
+	      @course = params[:course]
+	      @chosen_tags = params[:chosen_tags].split(",")
+
+	      # results will be calculated here using haaris' function in the model
+	      # right now i'll just use the existing basic search method
+
+      		@results = UniModule.basic_search(@chosen_tags) # this will change!
+
+	    else
+	     redirect_to "/"
+	    end
+
 	end
 
 end
