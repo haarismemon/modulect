8996f2661773b1da613df77dd6fd8de38d9404d2
k1502691 <vlad.nedelescu@kcl.ac.uk>
2017-02-17 12:32:41 +0000

    Add basic tests for course name validation

--- spec/requests/courses_spec.rb
+++ spec/requests/courses_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe "Courses", type: :request do
+  let(:course) { build(:course) }
 
+  describe "#valid?" do
+
+    context "when name is blank" do
+      before do
+        course.name = nil
+      end
+      it "evaluates to false" do
+        expect(course.valid?).to eq false
+      end
+    end
+
+    context "when name is already taken" do
+      before do
+        create(:course, name: course.name)
+      end
+      it "does have a name" do
+        expect(course.name).not_to be_blank
+      end
+      it "evaluates to false" do
+        expect(course.valid?).to eq false
+      end
+    end
+  end
 end
