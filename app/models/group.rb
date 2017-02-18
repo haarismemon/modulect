eaeb8bcf439b7d04fa5dc063a37ea63b2613390c
Haaris Memon <haaris.memon@kcl.ac.uk>
2017-02-18 16:41:15 +0000

    Set Group scope to take a module code instead of name

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,12 +1,12 @@
 class Group < ApplicationRecord
-  
+
   belongs_to :year_structure, optional: true
   has_and_belongs_to_many :uni_modules
   validates :name, presence: true
   validates :total_credits, presence: true
 
-  scope :search_by_module, lambda {|module_name|
-    joins(:uni_modules).where(["uni_modules.name = ?", module_name])
+  scope :search_by_module, lambda {|module_code|
+    joins(:uni_modules).where(["uni_modules.code = ?", module_code])
   }
 
 end
