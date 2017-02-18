bb17e4499378324ac6cba4e14f1c24ca6c92503c
Haaris Memon <haaris.memon@kcl.ac.uk>
2017-02-18 20:38:47 +0000

    Implement a helper method to returns list of tags associated to a course

--- app/helpers/pathway_search_helper.rb
+++ app/helpers/pathway_search_helper.rb
@@ -1,2 +1,27 @@
 module PathwaySearchHelper
+
+  # returns an array of all tags relating to a course
+  # input is a course object
+  # written by Haaris
+  def all_tags_of_course(course)
+    all_tags = []
+
+    year_structures = course.year_structures
+    # search through each year structure for groups of modules
+    year_structures.each do |year_structure|
+      groups = year_structure.groups
+      # search within each group of a year structure for modules
+      groups.each do |group|
+        modules = UniModule.all_modules_in_group(group)
+        # search within each module of a group for tags
+        modules.each do |uni_module|
+          all_tags.concat uni_module.tags
+        end
+      end
+    end
+
+    # returns an array of tags with no duplicates
+    all_tags.uniq!
+  end
+
 end
