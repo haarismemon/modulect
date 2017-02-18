5048114ba02da555339223d20eef55468862c79e
Haaris Memon <haaris.memon@kcl.ac.uk>
2017-02-18 15:48:29 +0000

    Add scope to find Groups with a UniModule

--- app/models/group.rb
+++ app/models/group.rb
@@ -5,4 +5,8 @@ class Group < ApplicationRecord
   validates :name, presence: true
   validates :total_credits, presence: true
 
+  scope :search_by_module, lambda {|module_name|
+    joins(:uni_modules).where(["uni_modules.name = ?", module_name])
+  }
+
 end
