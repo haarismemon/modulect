9833e35bec7a215da13005067cc4039d04785a71
Feras Al-Hamadani <feras.al-hamadani@kcl.ac.uk>
2017-02-19 21:57:47 +0000

    Fix incorrect module data being used bug

--- app/helpers/pathway_search_helper.rb
+++ app/helpers/pathway_search_helper.rb
@@ -3,7 +3,7 @@ module PathwaySearchHelper
   # returns a boolean if the next year of the course is defined and iterates the current year to that year
 	def exists_next_year_for(course_id, current_year)
 		if !YearStructure.where("course_id = ? AND year_of_study = ?", course_id, ++current_year).empty?
-      ++@year_of_study
+      @year_of_study = (@year_of_study.to_i + 1).to_s
 			true
 		else
 			false
