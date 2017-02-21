13bf166729f89767c95fee9bb36f89c9f72d2ab1
Omar <omar.rahman@kcl.ac.uk>
2017-02-21 23:51:30 +0000

    Reset user fields that are displayed to admin.

--- app/dashboards/user_dashboard.rb
+++ app/dashboards/user_dashboard.rb
@@ -36,10 +36,9 @@ class UserDashboard < Administrate::BaseDashboard
   # By default, it's limited to four items to reduce clutter on index pages.
   # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = [
-    :uni_modules,
-    :id,
     :first_name,
     :last_name,
+    :email
   ].freeze
 
   # SHOW_PAGE_ATTRIBUTES
