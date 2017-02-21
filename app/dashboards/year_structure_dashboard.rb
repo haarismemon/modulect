4483898ce145211846ed9abc4d3f3564f55db46e
Omar <omar.rahman@kcl.ac.uk>
2017-02-21 23:52:53 +0000

    Reset year structure that are displayed to admin.

--- app/dashboards/year_structure_dashboard.rb
+++ app/dashboards/year_structure_dashboard.rb
@@ -22,9 +22,7 @@ class YearStructureDashboard < Administrate::BaseDashboard
   # By default, it's limited to four items to reduce clutter on index pages.
   # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = [
-    :course,
     :groups,
-    :id,
     :year_of_study,
   ].freeze
 
