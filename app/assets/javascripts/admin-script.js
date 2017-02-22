2d950492e4dbe752097db767e337942210e1f6e1
Amin mnasour <amin.mansour@kcl.ac.uk>
2017-02-22 14:37:44 +0000

    Add a customizable script for admin

---
+++ app/assets/javascripts/admin-script.js
@@ -0,0 +1,5 @@
+
+ // Created by Amans on 22/02/2017.
+ //= require turbolinks
+ //= require_self
+
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,6 +4,7 @@
 Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w(your_stylesheet_name.css)
 Rails.application.config.assets.precompile += %w( admin-style.scss )
+Rails.application.config.assets.precompile += %w( admin-script.js )
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
