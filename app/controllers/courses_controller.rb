3acdec64e0d687eaa3a288410fff75f7d96e6cb4
Aqib Rashid <aqib.rashid@kcl.ac.uk>
2017-02-23 22:53:43 +0000

    Fix dropdown bug

---
+++ app/controllers/courses_controller.rb
@@ -0,0 +1,70 @@
+class CoursesController < ApplicationController
+
+  # GET /courses
+  # GET /courses.json
+  def index
+    #returns all tags by order of tag name
+    @courses = Course.alphabetically_order_by("name")
+  end
+
+  def show
+    @courses = Course.find(params[:id])
+  end
+
+  # GET /courses/new
+  def new
+    @course = Course.new
+  end
+
+  def edit
+    #! looks for object to ready populate form with the associated tag's data ready for modification by admin
+    @course = Course.find(params[:id])
+  end
+
+
+
+  def create
+    @course = Course.new(course_params)
+    # Save the object
+    if @course.save
+      # If save succeeds, redirect to the index action
+      flash[:notice] = @course.name+" was created successfully."
+      redirect_to(courses_path)
+    else
+      # If save fails, redisplay the form so user can fix problems
+      render('new')
+    end
+  end
+
+  def update
+    # Find a new object using form parameters
+    @course = Course.find(params[:id])
+    # Update the object
+    if @course.update_attributes(course_params)
+      # If save succeeds, redirect to the show action
+      flash[:notice] = @course.name+" was updated successfully."
+      redirect_to(courses_path)
+    else
+      # If save fails, redisplay the form so user can fix problems
+      render('edit')
+    end
+end
+
+
+  def destroy
+    #find by id
+    @course = Course.find(params[:id])
+    #delete tuple object from db
+    @course.destroy
+    flash[:notice] =  @course.name+"was deleted successfully."
+    #redirect to action which displays all courses
+    redirect_to(courses_path)
+  end
+
+  private
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def course_params
+      params.require(:course).permit(:name,:description)
+    end
+end
