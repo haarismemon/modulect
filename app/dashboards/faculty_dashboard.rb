e34263ad30f121f2ee05c196b1b17dc6a52bac2b
Amin mnasour <amin.mansour@kcl.ac.uk>
2017-02-24 00:01:48 +0000

    Rearrange faculty

--- app/dashboards/faculty_dashboard.rb
+++ app/dashboards/faculty_dashboard.rb
@@ -8,11 +8,12 @@ class FacultyDashboard < Administrate::BaseDashboard
   # which determines how the attribute is displayed
   # on pages throughout the dashboard.
   ATTRIBUTE_TYPES = {
-    departments: Field::HasMany,
-    id: Field::Number,
-    name: Field::String,
-    created_at: Field::DateTime,
-    updated_at: Field::DateTime,
+      name: Field::String,
+      departments: Field::HasMany,
+      id: Field::Number,
+
+      created_at: Field::DateTime,
+      updated_at: Field::DateTime,
   }.freeze
 
   # COLLECTION_ATTRIBUTES
@@ -21,26 +22,27 @@ class FacultyDashboard < Administrate::BaseDashboard
   # By default, it's limited to four items to reduce clutter on index pages.
   # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = [
-    :name,
-    :created_at
-    ].freeze
+      :name,
+      :created_at
+  ].freeze
 
   # SHOW_PAGE_ATTRIBUTES
   # an array of attributes that will be displayed on the model's show page.
   SHOW_PAGE_ATTRIBUTES = [
-    :departments,
-    :id,
-    :name,
-    :created_at,
-    :updated_at,
+      :id,
+      :name,
+      :departments,
+      :created_at,
+      :updated_at,
   ].freeze
 
   # FORM_ATTRIBUTES
   # an array of attributes that will be displayed
   # on the model's form (`new` and `edit`) pages.
   FORM_ATTRIBUTES = [
-    :departments,
-    :name,
+      :name,
+      :departments
+
   ].freeze
 
   # Overwrite this method to customize how faculties are displayed
