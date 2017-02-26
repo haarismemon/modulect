c91d1a1c24d2ccde184398b99f2b581cf2a00481
Amin mnasour <amin.mansour@kcl.ac.uk>
2017-02-26 04:35:43 +0000

    Add caching to forms

--- app/dashboards/course_dashboard.rb
+++ app/dashboards/course_dashboard.rb
@@ -12,10 +12,10 @@ class CourseDashboard < Administrate::BaseDashboard
     year_structures: Field::HasMany,
     id: Field::Number,
     name: Field::String,
-    description: Field::String,
+    description: DescriptionField,
+    year: YearField,
     created_at: Field::DateTime,
     updated_at: Field::DateTime,
-    year: Field::Number,
   }.freeze
 
   # COLLECTION_ATTRIBUTES
@@ -24,40 +24,41 @@ class CourseDashboard < Administrate::BaseDashboard
   # By default, it's limited to four items to reduce clutter on index pages.
   # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = [
+      :name,
     :departments,
     :year_structures,
-    :id,
-    :name,
+      :year
+
   ].freeze
 
   # SHOW_PAGE_ATTRIBUTES
   # an array of attributes that will be displayed on the model's show page.
   SHOW_PAGE_ATTRIBUTES = [
+      :id,
+      :name,
+      :description,
     :departments,
     :year_structures,
-    :id,
-    :name,
-    :description,
-    :created_at,
+      :year,
+  :created_at,
     :updated_at,
-    :year,
   ].freeze
 
   # FORM_ATTRIBUTES
   # an array of attributes that will be displayed
   # on the model's form (`new` and `edit`) pages.
   FORM_ATTRIBUTES = [
+      :name,
+      :description,
     :departments,
     :year_structures,
-    :name,
-    :description,
     :year,
   ].freeze
 
   # Overwrite this method to customize how courses are displayed
   # across all pages of the admin dashboard.
   #
-  # def display_resource(course)
-  #   "Course ##{course.id}"
-  # end
+   def display_resource(course)
+     course.name
+  end
 end
