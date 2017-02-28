1f604092cc352ba8e2fd32131369c05479a1bfb2
Aqib Rashid <aqib.rashid@kcl.ac.uk>
2017-02-28 22:30:24 +0000

    Change call to helper, by adding has_many relations in course

--- app/models/course.rb
+++ app/models/course.rb
@@ -9,6 +9,9 @@ class Course < ApplicationRecord
   has_and_belongs_to_many :departments
   has_many :year_structures
 
+  has_many :groups, through: :year_structures
+  has_many :uni_modules, through: :groups
+
   # Registers a department as belonging to this course.
   def add_department(valid_department)
     departments << valid_department
