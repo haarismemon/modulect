c68862f66f5d75da6276b730c9947d730972a462
Aqib Rashid <aqib.rashid@kcl.ac.uk>
2017-03-03 16:33:44 +0000

    Revert career search controller

--- app/controllers/career_search_controller.rb
+++ app/controllers/career_search_controller.rb
@@ -8,21 +8,17 @@ class CareerSearchController < ApplicationController
 
     	if logged_in?
     		@user = current_user
-
 		    #Initialise departments and courses to be empty unless previously selected
-        if(@user.department)
-          @departments = @user.faculty.departments
+		     if(@user.department_id.present?)
+		      @departments = Faculty.find_by_id(@user.faculty_id).departments
 		    else
 		      @departments = {}
 		    end
-
-		    if(@user.department && @user.course)
+		    if(@user.department_id.present? && @user.course_id.present?)
 		      @courses = Department.find_by_id(@user.department_id).courses
 		    else
 		      @courses = {}
 		    end
-      else
-        redirect_to login_path
 		end
 	end
 
