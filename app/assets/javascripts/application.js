bd1143d8b61732471da4d10f97238f88b13759d4
Amin mnasour <amin.mansour@kcl.ac.uk>
2017-03-03 21:35:03 +0000

    Revert application.js to original

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
+//= require jquery_ujs
 //= require bootstrap-tagsinput
 //= require twitter/typeahead.min
 //= require turbolinks
 //= require_tree ./user-side
-//= require jquery
-//= require jquery_ujs
