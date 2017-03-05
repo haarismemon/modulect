eaf37978cefe0571f1c6f458506ecbfc0804bc2a
k1502691 <vlad.nedelescu@kcl.ac.uk>
2017-03-05 18:21:34 +0000

    Loosen constraint of department belonging to a faculty as optional

--- app/models/department.rb
+++ app/models/department.rb
@@ -4,7 +4,7 @@ class Department < ApplicationRecord
   # has_and_belongs_to_many :users
   has_and_belongs_to_many :courses
   has_and_belongs_to_many :uni_modules
-  belongs_to :faculty
+  belongs_to :faculty, optional: true
   # Registers a course as belonging to this department.
   def add_course(valid_course)
     courses << valid_course
