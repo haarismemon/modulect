5f0858edaa44d6d57794bc98581f81135e0078b4
Aqib Rashid <aqib.rashid@kcl.ac.uk>
2017-03-06 18:12:52 +0000

    Add base controller

--- app/controllers/modulect_admin/base_controller.rb
+++ app/controllers/modulect_admin/base_controller.rb
@@ -1,8 +1,25 @@
 module ModulectAdmin
 
 	class ModulectAdmin::BaseController < ApplicationController
+	before_action :authenticate_admin
 
 	 layout "modulect_admin/modulect_admin"
+
+   		include SessionsHelper
+
+	    def authenticate_admin
+	      
+	      # if not an admin, display a flash
+	      if logged_in? && !admin_user
+	         redirect_to root_path
+	         flash[:error] = "You are not logged in as an Administator. Please try again."
+	      # else go through old process of logging in
+	      elsif !admin_user
+	        store_location
+	        redirect_to admin_login_path
+	      end
+	    end
+
 
 	end
 	
