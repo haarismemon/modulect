44821c841ca22f3d0644477dc6314cf951441f57
Aqib Rashid <aqib.rashid@kcl.ac.uk>
2017-03-06 21:39:31 +0000

    Add controller actions

--- app/controllers/admin/uni_modules_controller.rb
+++ app/controllers/admin/uni_modules_controller.rb
@@ -3,22 +3,32 @@ module Admin
     
         
   	def index
+      @uni_modules = UniModule.paginate(page: params[:page])
   	end
 
   	def new
+      @uni_module = UniModule.new
   	end
 
+    # For Feras
   	def create
   	end
 
 
+    # For Feras
   	def edit
   	end
 
+
+    # For Feras
   	def update
+
   	end
 
   	def destroy
+      UniModule.find(params[:id]).destroy
+      flash[:success] = "Module successfully deleted."
+      redirect_to admin_uni_modules_path
   	end
 
   end
