715c849f1f81f39890cd27ec86c3fde9fcc1a870
Haaris Memon <haaris.memon@kcl.ac.uk>
2017-03-06 17:58:56 +0000

    Get resource object from administrate resources list

--- app/controllers/admin/upload_controller.rb
+++ app/controllers/admin/upload_controller.rb
@@ -10,6 +10,19 @@ module Admin
 
       resource = nil
 
+      Administrate::Namespace.new(namespace).resources.each do |administrate_resource|
+        if administrate_resource.to_s == resource_name
+          resource = administrate_resource
+        end
+      end
+
+      logger.debug(resource)
+
+      # CSV.open("#{resource_name}_upload.csv") do |csv|
+      #   csv << ["animal", "count", "price"]
+      #   csv << ["fox", "1", "$90.00"]
+      # end
+
       head :no_content
     end
 
