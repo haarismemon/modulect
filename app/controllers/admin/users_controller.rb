774e12ea007b5ed3a7f941d65793a73f3de7ee2c
Amin mnasour <amin.mansour@kcl.ac.uk>
2017-03-07 01:22:12 +0000

    Finish update action in admin/users controller

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -26,5 +26,27 @@ module Admin
       @user = User.find(params[:id])
     end
 
+    def update
+      # Find a  object using id parameters
+      @user = User.find(params[:id])
+      # Update the object
+      if @user.update_attributes(user_params)
+        # If save succeeds, redirect to the index action
+        flash[:success] = "Successfully updated "+ @user.full_name
+        redirect_to(admin_user_path(@user)) and return
+      else
+        # If save fails, redisplay the form so user can fix problems
+        render('edit')
+      end
+    end
+
+
+    private
+
+    def user_params
+      #!add params that want to be recognized by this application
+      params.require(:user).permit(:first_name, :last_name, :email, :password, :username, :year_of_study,:user_level)
+    end
+
   end
 end
