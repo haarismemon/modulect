c558e7607e98c9dca09b7623eaa08a03049d2741
Aqib Rashid <aqib.rashid@kcl.ac.uk>
2017-03-07 12:41:29 +0000

    Implement search, yet to add to search bar

--- app/controllers/admin/uni_modules_controller.rb
+++ app/controllers/admin/uni_modules_controller.rb
@@ -1,26 +1,24 @@
 module Admin
   class UniModulesController < Admin::BaseController
-        
+  require 'will_paginate/array'
+
+
   	def index      
 
       @uni_modules = UniModule.all
-
     
-     # if params[:search].present?
-     #   @uni_modules = @uni_modules.select { |uni_module| uni_module.name.include?(params[:search]) }
-     # end
+      if params[:search].present?
+        @uni_modules = @uni_modules.select { |uni_module| uni_module.name.downcase.include?(params[:search].downcase) }
+        @uni_modules = @uni_modules.sort_by{|uni_module| uni_module[:name]}
+      end
     
 
       # if sorting present
-      if params[:sortby].present? && params[:order].present?
+      if params[:sortby].present? && params[:order].present? && !params[:search].present?
         @uni_modules = sort(UniModule, @uni_modules, params[:sortby], params[:order], 20)
       else
-        @uni_modules = @uni_modules.paginate(page: params[:page], :per_page => 20).order('name ASC')
+        @uni_modules = @uni_modules.paginate(page: params[:page], :per_page => 20)
       end
-
-
-
-
 
   	end
 
