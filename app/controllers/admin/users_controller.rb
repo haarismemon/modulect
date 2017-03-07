fe942dec2c700141a456ee08502fbacd0934ce80
Amin mnasour <amin.mansour@kcl.ac.uk>
2017-03-07 01:20:18 +0000

    Finish new action in admin/users controller

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,30 +1,10 @@
 module Admin
-	class UsersController < Admin::BaseController
+  class UsersController < ApplicationController
+
+    def new
+      @user = User.new
+    end
 
 
-	    
-  	def index
-      @users = User.all
-  	end
-
-  	def new
-  	end
-
-  	def create
-  	end
-
-
-  	def edit
-  	end
-
-  	def update
-  	end
-
-  	def destroy
-  	end
-
- 
-
-
-	end
+  end
 end
