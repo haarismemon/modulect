f9886bd0812538cb27ae31cc69a1d257416fa79c
Amin mnasour <amin.mansour@kcl.ac.uk>
2017-03-07 01:20:58 +0000

    Finish create action in admin/users controller

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -5,6 +5,22 @@ module Admin
       @user = User.new
     end
 
+    def create
+      # Instantiate a new object using form parameters
+      @user = User.new(user_params)
+      # Save the object
+      if @user.save
+        # If save succeeds, redirect to the index action
+        flash[:notice] = "You have successfully created #{@user.full_name} and it's privileges have been granted"
+        redirect_to(users_path)
+      else
+        # If save fails, redisplay the form so user can fix problems
+        render(:new)
+
+      end
+    end
+
+
 
   end
 end
