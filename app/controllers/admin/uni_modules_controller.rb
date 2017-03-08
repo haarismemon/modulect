0933f14b9ec85a05238c43e1bb158cb18dda4913
Aqib Rashid <aqib.rashid@kcl.ac.uk>
2017-03-08 23:33:59 +0000

    Modify the bulk action logic to show an alert followed by page load

--- app/controllers/admin/uni_modules_controller.rb
+++ app/controllers/admin/uni_modules_controller.rb
@@ -52,9 +52,24 @@ module Admin
 
   	def destroy
       @uni_module = UniModule.find(params[:id])
-      @uni_module.destroy
-      flash[:success] = "Module successfully deleted"
-      redirect_back_or admin_uni_modules_path
+      can_delete = true
+
+      # check if being used
+      Group.all.each do |group|
+        if group.uni_modules.include?(@uni_module)
+          can_delete = false
+          break
+        end
+      end
+
+      if can_delete
+        @uni_module.destroy
+        flash[:success] = "Module successfully deleted"
+        redirect_back_or admin_uni_modules_path
+      else 
+        flash[:error] = "Module is linked to a course, remove from course first"
+        redirect_back_or admin_uni_modules_path
+      end
 
   	end
 
@@ -68,11 +83,23 @@ module Admin
       uni_module = UniModule.find(id.to_i)
 
       if !uni_module.nil?
-        uni_module.update_attribute("name", "AZZ")
+
+      Group.all.each do |group|
+        if group.uni_modules.include?(uni_module)
+          can_delete = false
+          break
+        end
+      end
+
+      if can_delete
+        uni_module.destroy
+      end
+
+
       end
     end
 
-    render :js => "window.location = '/admin/uni_modules?ba=true'"
+    head :no_content
 
   end
 
