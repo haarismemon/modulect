53ade936f247e685e6f32aa92d9d647ff861efad
Aqib Rashid <aqib.rashid@kcl.ac.uk>
2017-03-09 21:34:56 +0000

    Remove redundant heading

--- config/routes.rb
+++ config/routes.rb
@@ -98,7 +98,6 @@ Rails.application.routes.draw do
 
 
   # ERROR PAGES
-  # Error pages
   match "/404", :to => "errors#not_found", :via => :all
   match "/500", :to => "errors#internal_server_error", :via => :all
 
