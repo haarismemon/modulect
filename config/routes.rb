37b6aff05c2df523f20963ccbefd42f20468ca99
Aqib Rashid <aqib.rashid@kcl.ac.uk>
2017-03-09 21:31:57 +0000

    Clean up routes, remove duplicates, reorganised

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
 
-  get 'career_search/choose'
-  get 'career_search/view'
-  get 'errors/not_found'
-  get 'errors/internal_server_error'
-
+  # ADMIN
   namespace :admin do
     resources :courses do
       resources :year_structures, only: :edit
@@ -20,20 +16,27 @@ Rails.application.routes.draw do
     resources :interest_tags, except: [:show]
     get 'upload', to: 'upload#upload'
 
+    # BULK ACTIONS
     post '/uni_modules/bulk_delete', to: 'uni_modules#bulk_delete'
 
     root to: "dashboard#index"
   end
 
 
-  # General
+  # GENERAL
   root 'search#home'
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
   get '/search', to: 'search#home'
   get '/saved', to: 'saved#view'
   get '/admin', to: 'admin#dashboard'
+  get 'career_search/choose'
+  get 'career_search/view'
+  get 'errors/not_found'
+  get 'errors/internal_server_error'
 
+
+  # USERS & AUTHENTICATION
   # Authentication
   get     '/login',   to: 'sessions#new'
   get     '/admin/login',   to: 'sessions#new'
@@ -43,32 +46,6 @@ Rails.application.routes.draw do
   # Signup
   get '/signup', to: 'users#new', as: 'signup'
   post 'signup', to: 'users#create'
-
-  # Uni Modules
-  resources :uni_modules, only: [:show]
-
-  # Search
-  get 'search/pathway_search'
-  get 'search/view_results'
-
-  # Pathway search
-  get '/pathway-search', to: 'pathway_search#begin'
-  get 'pathway-search/begin'
-  get 'pathway-search/choose'
-  get 'pathway-search/view_results'
-
-  # Career search
-  get '/career-search', to: 'career_search#begin'
-  get 'career-search/begin'
-  get 'career-search/choose'
-  get 'career-search/view_results'
-
-  # Save pathways and modules, and create tags used in ajax
-  post 'application/save_module'
-  post 'application/save_pathway'
-  post 'application/delete_pathway'
-  post 'admin/add_new_tag',   to: 'admin/tags#add_new_tag'
-  post 'admin/add_new_faculty', to: 'admin/faculties#add_new_faculty'
 
   # Profile
   get '/*all/update_departments', to: 'users#update_departments', defaults: { format: 'js' }
@@ -86,6 +63,8 @@ Rails.application.routes.draw do
   # Account activations
   resources :account_activations, only: [:edit]
 
+
+  # SEARCH
   # Search
   get 'search/pathway_search'
   get 'search/view_results'
@@ -97,6 +76,28 @@ Rails.application.routes.draw do
   get 'pathway-search/view_results'
   get 'pathway-search/view_results_test'
 
+
+  # Career search
+  get '/career-search', to: 'career_search#begin'
+  get 'career-search/begin'
+  get 'career-search/choose'
+  get 'career-search/view_results'
+
+  # UNI MODULES
+  # Uni Modules
+  resources :uni_modules, only: [:show]
+
+
+  # AJAX
+  # Save pathways and modules, and create tags used in ajax
+  post 'application/save_module'
+  post 'application/save_pathway'
+  post 'application/delete_pathway'
+  post 'admin/add_new_tag',   to: 'admin/tags#add_new_tag'
+  post 'admin/add_new_faculty', to: 'admin/faculties#add_new_faculty'
+
+
+  # ERROR PAGES
   # Error pages
   match "/404", :to => "errors#not_found", :via => :all
   match "/500", :to => "errors#internal_server_error", :via => :all
