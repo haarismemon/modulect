6a20a98dee7cdf5bc1f845ace04585971a7cf8c7
Khalil Ahmed <mohamad.ahmed@kcl.ac.uk>
2017-03-09 01:42:06 +0000

    Add inline comments to download controller method

--- app/controllers/admin/upload_controller.rb
+++ app/controllers/admin/upload_controller.rb
@@ -5,17 +5,22 @@ module Admin
     end
 
     def download
+      # Retrieve the resource requested to upload e.g Users
       resource_name = params[:resource_choice]
+      # Will store verified resource
       resource = nil
 
+      # Verify that the resource with the corresponding name exists
       Administrate::Namespace.new(namespace).resources.each do |administrate_resource|
         if administrate_resource.to_s == resource_name
           resource = administrate_resource
         end
       end
 
+      # Will store the attribute names of the resource
       resource_header = []
 
+      # Retrieves the attribute names depending on resource
       case resource.to_s
         when "courses"
           resource_header = Course.attribute_names
@@ -31,15 +36,19 @@ module Admin
           resource_header = YearStructure.attribute_names
       end
 
+      # Create a CSV file in the specified path
       require 'csv'
       CSV.open("app/assets/admin_upload.csv", "wb") do |csv|
+        # Add the attribute names as the header/first row
         csv << resource_header
       end
 
+      # Debugging to check file has been generated correctly
       # CSV.foreach("admin_upload.csv") do |row|
       #   logger.debug(row)
       # end
 
+      # Send the file for download
       send_file "app/assets/admin_upload.csv", :type=>"text/csv", :x_sendfile=>true
     end
 
