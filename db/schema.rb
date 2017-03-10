01d3b6d1dd11a87b4b6cd4e059d8c463373e648f
Haaris Memon <haaris.memon@kcl.ac.uk>
2017-03-10 23:48:02 +0000

    Migrate Comments table to schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170308151326) do
+ActiveRecord::Schema.define(version: 20170310234142) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "commenter"
+    t.text     "body"
+    t.integer  "uni_module_id"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+    t.index ["uni_module_id"], name: "index_comments_on_uni_module_id"
+  end
 
   create_table "courses", force: :cascade do |t|
     t.string   "name"
