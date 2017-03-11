d9faf963d75a7478a7eb34629c781d53be11dfdb
Haaris Memon <haaris.memon@kcl.ac.uk>
2017-03-11 12:45:01 +0000

    Paginate the comments on module page with 5 per page

--- app/controllers/uni_modules_controller.rb
+++ app/controllers/uni_modules_controller.rb
@@ -2,6 +2,7 @@ class UniModulesController < ApplicationController
 
   def show
     @uni_module = UniModule.find(params[:id])
+    @comments = @uni_module.comments.order("created_at DESC").page(params[:page]).per(5)
   end
 
 end
