9a706a41524bec58121e2bf2cbee83fa85351e3e
Haaris Memon <haaris.memon@kcl.ac.uk>
2017-03-11 01:16:51 +0000

    Add rating to the review section on module page

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :uni_module
 
+  validates :commenter, :presence => true
   validates :rating, :inclusion => 1..5
+  validates :body, :presence => true
 end
