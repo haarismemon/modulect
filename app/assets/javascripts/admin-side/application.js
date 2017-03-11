2434e10e1f626bc029cace5d7e5900cc50da5b75
Amin mnasour <amin.mansour@kcl.ac.uk>
2017-03-11 18:48:22 +0000

    Replace has-many field in forms and make inputs same size

--- app/assets/javascripts/admin-side/application.js
+++ app/assets/javascripts/admin-side/application.js
@@ -2,8 +2,21 @@
 //= require jquery_ujs
 //= require bootstrap
 //= require cocoon
-//= require selectize
+//= require_self
+//= require admin-side/selectize.min
 //= require moment
 //= require datetime_picker
 //= require_tree
 //= require twitter/typeahead.min
+
+function intializeSelectize() {
+
+    $('.selectize').selectize({
+        plugins: ['remove_button'],
+        delimiter: ',',
+        persist: false,
+        create: false
+    });
+}
+
+
