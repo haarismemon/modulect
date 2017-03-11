4c28f6b58aa8bfbcad0c15cdbbf12160df87a49a
Haaris Memon <haaris.memon@kcl.ac.uk>
2017-03-11 00:15:21 +0000

    Add create action for a comment for a uni module

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,11 @@
 class CommentsController < ApplicationController
+
+  def create
+    @uni_module = UniModule.find(params[:uni_module_id])
+
+    @comment = @uni_module.comments.create(comment_params)
+
+    redirect_to uni_module_path(@uni_module)
+  end
+
 end
