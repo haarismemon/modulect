2314ec08a4809b5f9f67d3adedef09c4ab565bd4
Aqib Rashid <aqib.rashid@kcl.ac.uk>
2017-03-16 18:01:53 +0000

    Update controller to now use the time period, department and course from query

--- app/controllers/admin/analytics_controller.rb
+++ app/controllers/admin/analytics_controller.rb
@@ -2,8 +2,29 @@ module Admin
 	class AnalyticsController < Admin::BaseController
 
 		include AnalyticsHelper
+		include SessionsHelper
 
 		def analytics
+			possible_times = ["day","week","month","year"]
+			if params[:time].present? && possible_times.include?(params[:time])
+				@time_period = params[:time]
+			else
+				@time_period = "month"
+			end
+
+			if current_user.user_level == "department_admin_access"
+				@department = current_user.department_id.to_s
+			elsif params[:department].present? && Department.exists?(params[:department].to_i) && current_user.user_level == "super_admin_access"
+				@department = params[:department]
+			elsif current_user.user_level == "super_admin_access"
+				@department == "any"
+			end
+
+			if params[:course].present? && Course.exists?(params[:course].to_i)
+				@course = Course.find(params[:course].to_i)
+			else
+				@course = "any"
+			end
 		end
 		
 	end
