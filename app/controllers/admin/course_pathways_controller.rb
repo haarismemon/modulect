89665662811a4f8b81d13cd64e73fb3a2247b969
Aleksandr Sutin <aleksandr.sutin@kcl.ac.uk>
2017-03-18 03:03:55 +0000

    fit admin style

--- app/controllers/admin/course_pathways_controller.rb
+++ app/controllers/admin/course_pathways_controller.rb
@@ -1,5 +1,7 @@
-class Admin::CoursePathwaysController < ApplicationController
-	def edit
-		@course = Course.find_by(id: params[:id])
+module Admin
+	class CoursePathwaysController < Admin::BaseController
+		def edit
+			@course = Course.find_by(id: params[:id])
+		end
 	end
 end
