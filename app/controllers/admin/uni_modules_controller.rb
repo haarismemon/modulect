4df0d614d03919338c7fc2404bb8729aa667ffb9
Omar <omar.rahman@kcl.ac.uk>
2017-03-18 21:45:07 +0000

    Solve merge conflict and formatting.

--- app/controllers/admin/uni_modules_controller.rb
+++ app/controllers/admin/uni_modules_controller.rb
@@ -6,18 +6,18 @@ module Admin
       redirect_to edit_admin_uni_module_path(params[:id])
     end
 
-  	def index
+    def index
 
-      if current_user.user_level == "super_admin_access"     
+      if current_user.user_level == "super_admin_access"
         if params[:dept].present? && params[:dept].to_i != 0 && Department.exists?(params[:dept].to_i)
           @dept_filter_id = params[:dept].to_i
           @uni_modules = Department.find(@dept_filter_id).uni_modules
         else
-          @uni_modules = UniModule.all 
+          @uni_modules = UniModule.all
         end
       else
         @uni_modules = Department.find(current_user.department_id).uni_modules
-      end  
+      end
 
       if params[:per_page].present? && params[:per_page].to_i > 0
         @per_page = params[:per_page].to_i
@@ -33,20 +33,20 @@ module Admin
         # perhaps they were looking for a code
         if @uni_modules.size == 0
           @uni_modules = UniModule.all.select { |uni_module| uni_module.code.downcase.include?(params[:search].downcase) }.sort_by{|uni_module| uni_module[:name]}
-        end 
+        end
         @uni_modules = Kaminari.paginate_array(@uni_modules).page(params[:page]).per(@per_page)
 
       elsif params[:sortby].present? && params[:order].present? && !params[:search].present?
         @sort_by = params[:sortby]
         @order = params[:order]
         @uni_modules = sort(UniModule, @uni_modules, @sort_by, @order, @per_page, "name")
-       @uni_modules = Kaminari.paginate_array(@uni_modules).page(params[:page]).per(@per_page)
+        @uni_modules = Kaminari.paginate_array(@uni_modules).page(params[:page]).per(@per_page)
 
 
       else
         @uni_modules = @uni_modules.order('LOWER(name) ASC').page(params[:page]).per(@per_page)
       end
-     
+
       @uni_modules_to_export = @uni_modules
       if params[:export].present?
         export_module_ids_string = params[:export]
@@ -58,7 +58,7 @@ module Admin
         end
 
         @uni_modules_to_export = UniModule.where(id: export_module_ids)
-        @uni_modules_to_export = @uni_modules_to_export.order('LOWER(name) ASC')  
+        @uni_modules_to_export = @uni_modules_to_export.order('LOWER(name) ASC')
       else
         @uni_modules_to_export = @uni_modules
       end
@@ -68,18 +68,18 @@ module Admin
         format.csv {send_data @uni_modules_to_export.to_csv}
       end
 
-  	end
+    end
 
-  	def new
+    def new
       @uni_module = UniModule.new
 
       @departments = []
       @careerTags = []
       @interestTags = []
       @required = []
-  	end
+    end
 
-  	def create
+    def create
       @uni_module = UniModule.new(uni_module_params)
       if @uni_module.save
         # If save succeeds, redirect to the index action
@@ -90,10 +90,10 @@ module Admin
         render(:new)
       end
 
-  	end
+    end
 
 
-  	def edit
+    def edit
       if params[:id].present?
         @uni_module = UniModule.find(params[:id])
 
@@ -103,9 +103,9 @@ module Admin
         @interestTags = @uni_module.interest_tags.pluck(:name)
         @required = @uni_module.uni_modules.pluck(:name)
       end
-  	end
+    end
 
-  	def update
+    def update
       @uni_module = UniModule.find(params[:id])
       if params[:uni_module][:career_tags].present? && !params[:uni_module][:career_tags].empty? && params[:uni_module][:interest_tags].present? && !params[:uni_module][:interest_tags].empty? && params[:uni_module][:department_ids].present? && !params[:uni_module][:department_ids].empty? && @uni_module.update_attributes(uni_module_params)
         @uni_module.departments.clear()
@@ -154,7 +154,7 @@ module Admin
         interest_tags = params[:uni_module][:interest_tags].split(',')
         interest_tags.each do |tag|
           chosen_tag = Tag.find_by_name(tag)
-           # Add the interest tag association
+          # Add the interest tag association
           if chosen_tag.present?
             @uni_module.tags << chosen_tag
           else
@@ -170,7 +170,7 @@ module Admin
         flash[:success] = "Successfully updated #{@uni_module.name}"
         redirect_to(edit_admin_uni_module_path(@uni_module)) and return
 
-      else 
+      else
         # Failed to update
         # If save fails, redisplay the form so user can fix problems
         if !params[:uni_module][:department_ids].present? || params[:uni_module][:department_ids].empty?
@@ -189,7 +189,7 @@ module Admin
         render(:edit)
       end
 
-  	end
+    end
 
     def generate_tags
       uri = URI.parse("https://api.thomsonreuters.com/permid/calais")
@@ -213,7 +213,7 @@ module Admin
       render :json => http.request(request).body
     end
 
-  	def destroy
+    def destroy
       @uni_module = UniModule.find(params[:id])
       can_delete = true
 
@@ -230,47 +230,47 @@ module Admin
         tag_clean_up
         flash[:success] = "Module successfully deleted"
         redirect_to admin_uni_modules_path
-      else 
+      else
         flash[:error] = "Module is linked to a course, remove from course first"
         redirect_to admin_uni_modules_path
       end
 
-  	end  
-
-  def bulk_delete
-    module_ids_string = params[:ids]
-    module_ids = eval(module_ids_string)
-
-    module_ids.each do |id|
-      uni_module = UniModule.find(id.to_i)
-      can_delete = true
-      if !uni_module.nil?
-
-        Group.all.each do |group|
-          if group.uni_modules.include?(uni_module)
-            can_delete = false
-            break
-          end
-        end
-        if can_delete
-         uni_module.destroy
-        end
-
-      end
-      #uni_module.update_attribute("name", id)
-
     end
 
-    head :no_content
+    def bulk_delete
+      module_ids_string = params[:ids]
+      module_ids = eval(module_ids_string)
 
-  end
+      module_ids.each do |id|
+        uni_module = UniModule.find(id.to_i)
+        can_delete = true
+        if !uni_module.nil?
+
+          Group.all.each do |group|
+            if group.uni_modules.include?(uni_module)
+              can_delete = false
+              break
+            end
+          end
+          if can_delete
+            uni_module.destroy
+          end
+
+        end
+        #uni_module.update_attribute("name", id)
+
+      end
+
+      head :no_content
+
+    end
 
     def comments
       @uni_module = UniModule.find(params[:id])
       @comments = @uni_module.comments
     end
 
-   private
+    private
     def uni_module_params
       params.require(:uni_module).permit(:name, :code, :description, :semester, :credits, :lecturers, :assessment_methods, :assessment_dates, :exam_percentage, :coursework_percentage, :pass_rate, :more_info_link)
     end
@@ -300,6 +300,6 @@ module Admin
       end
     end
 
-end
- 
+  end
+
 end
