3147d5bdb977e75f31d78bfa44b2d9fb4341d65d
Khalil Ahmed <mohamad.ahmed@kcl.ac.uk>
2017-03-19 06:58:20 +0000

    Re-order actions in order of call

--- app/controllers/admin/upload_controller.rb
+++ app/controllers/admin/upload_controller.rb
@@ -5,6 +5,63 @@ module Admin
     def upload
     end
 
+    def download
+      # Retrieve the resource requested to upload e.g Users
+      resource_name = params[:resource_choice]
+      # Will store verified resource
+      resource = nil
+
+      # Verify that the resource with the corresponding name exists
+      %w(courses uni_modules departments faculties).each do |verify_resource|
+        if verify_resource.to_s == resource_name
+          resource = verify_resource
+        end
+      end
+
+      # Store the resource as a session variable
+      session[:resource_name] = resource.to_s
+
+      # Retrieves the attribute names depending on resource
+      resource_header = resource.to_s.classify.constantize.attribute_names
+
+      # Remove attributes that shouldn't appear on CSV
+      to_remove = %w(id created_at updated_at)
+      resource_header = resource_header - to_remove
+
+      # Store resource header as session variable
+      session[:resource_header] = resource_header
+
+      # Add specific association attributes
+      # Add additional header attribute for the departments of the faculty/courses
+      if resource.to_s == 'faculties' || resource.to_s == 'courses'
+        resource_header << 'departments'
+      end
+
+      if resource.to_s == 'uni_modules'
+        # Add attribute for departments and prerequisite_modules
+        resource_header << 'departments'
+        resource_header << 'prerequisite_modules'
+      end
+
+      # Replace faculty_id attribute with faculty_name for departments
+      if resource.to_s == 'departments'
+        # Remove faculty_id attribute
+        resource_header.delete('faculty_id')
+        # Add faculty_name
+        resource_header << 'faculty_name'
+      end
+
+      # Create a CSV file in the specified path
+      file_name = "app/assets/#{resource.to_s}_upload.csv"
+      CSV.open(file_name, 'wb') do |csv|
+        # Add the attribute names as the header/first row
+        csv << resource_header
+      end
+
+      # Send the file for download
+      send_file file_name, type: 'text/csv', x_sendfile: true
+    end
+
     def upload_csv
       # Retrieve csv file that was uploaded
       uploaded_csv = params[:csv_upload]
@@ -102,63 +159,6 @@ module Admin
       end
 
       redirect_to :back
-    end
-
-    def download
-      # Retrieve the resource requested to upload e.g Users
-      resource_name = params[:resource_choice]
-      # Will store verified resource
-      resource = nil
-
-      # Verify that the resource with the corresponding name exists
-      %w(courses uni_modules departments faculties).each do |verify_resource|
-        if verify_resource.to_s == resource_name
-          resource = verify_resource
-        end
-      end
-
-      # Store the resource as a session variable
-      session[:resource_name] = resource.to_s
-
-      # Retrieves the attribute names depending on resource
-      resource_header = resource.to_s.classify.constantize.attribute_names
-
-      # Remove attributes that shouldn't appear on CSV
-      to_remove = %w(id created_at updated_at)
-      resource_header = resource_header - to_remove
-
-      # Store resource header as session variable
-      session[:resource_header] = resource_header
-
-      # Add specific association attributes
-      # Add additional header attribute for the departments of the faculty/courses
-      if resource.to_s == 'faculties' || resource.to_s == 'courses'
-        resource_header << 'departments'
-      end
-
-      if resource.to_s == 'uni_modules'
-        # Add attribute for departments and prerequisite_modules
-        resource_header << 'departments'
-        resource_header << 'prerequisite_modules'
-      end
-
-      # Replace faculty_id attribute with faculty_name for departments
-      if resource.to_s == 'departments'
-        # Remove faculty_id attribute
-        resource_header.delete('faculty_id')
-        # Add faculty_name
-        resource_header << 'faculty_name'
-      end
-
-      # Create a CSV file in the specified path
-      file_name = "app/assets/#{resource.to_s}_upload.csv"
-      CSV.open(file_name, 'wb') do |csv|
-        # Add the attribute names as the header/first row
-        csv << resource_header
-      end
-
-      # Send the file for download
-      send_file file_name, type: 'text/csv', x_sendfile: true
     end
 
   end
