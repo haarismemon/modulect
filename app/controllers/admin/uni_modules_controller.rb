3b209f21cfa08c5ca8614d6b16402ad00e035392
Aqib Rashid <aqib.rashid@kcl.ac.uk>
2017-03-19 21:58:38 +0000

    Fix the sorting

--- app/controllers/admin/uni_modules_controller.rb
+++ app/controllers/admin/uni_modules_controller.rb
@@ -268,14 +268,14 @@ module Admin
     def comments
       @uni_module = UniModule.find(params[:id])
       @comments = @uni_module.comments
-
+      
       if params[:per_page].present? && params[:per_page].to_i > 0
         @per_page = params[:per_page].to_i
       else
         @per_page = 20
       end
 
-      if params[:sortby].present? && params[:order].present? && !params[:search].present?
+      if params[:sortby].present? && params[:order].present? 
         @sort_by = params[:sortby]
         @order = params[:order]
         @comments = sort(Comment, @comments, @sort_by, @order, @per_page, "created_at")
