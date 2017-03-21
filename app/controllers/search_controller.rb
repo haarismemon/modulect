934b1337bd94add8d42033a46b014e370916598a
Amin mnasour <amin.mansour@kcl.ac.uk>
2017-03-21 01:04:32 +0000

    revert back to old auto-delete feature for notice

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,15 +4,15 @@ class SearchController < ApplicationController
     retrieve_notices
     @tag_names = Tag.pluck(:name)
     @module_names = UniModule.pluck(:name)
-    @module_code = UniModule.pluck(:code) 
+    @module_code = UniModule.pluck(:code)
   end
 
   def view_results
     @tag_names = Tag.pluck(:name)
     @module_names = UniModule.pluck(:name)
-    @module_code = UniModule.pluck(:code) 
+    @module_code = UniModule.pluck(:code)
     @results = []
-    if params.has_key?(:chosen_tags) && !params[:chosen_tags].empty? 
+    if params.has_key?(:chosen_tags) && !params[:chosen_tags].empty?
       @temp_array = params[:chosen_tags].split(",")
 
       @temp_array.each do |tag_name|
@@ -39,15 +39,15 @@ class SearchController < ApplicationController
       # if user doesn't have a department, retrieve global notices
       @notices = Notice.all.where(:department_id => nil).where(:broadcast => true).where(['live_date<= ?', DateTime.now])
     else
-      auto_delete_notices
       # if user has a department , display local notices
       @notices = Notice.all.where(:department_id => [@current_user.department_id, nil]).where(:broadcast => true).where(['live_date<= ?', DateTime.now])
+      auto_delete_notices
     end
   end
 
   # deletes notices which are past their expiry date
   def auto_delete_notices
-    Notice.all.where(:department_id => [@current_user.department_id, nil]).each { |obj|
+    @notices.each { |obj|
       if obj.auto_delete && !obj.end_date.nil? && obj.end_date.past?
         obj.destroy
       end
