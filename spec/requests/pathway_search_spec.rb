87c7953494793573d7d7e86b954d665ed9f95b98
k1502691 <vlad.nedelescu@kcl.ac.uk>
2017-03-22 12:57:13 +0000

    Clean up a bit

--- spec/requests/pathway_search_spec.rb
+++ spec/requests/pathway_search_spec.rb
@@ -60,50 +60,8 @@ feature "Student searching for module pathway", :js => true do
   given(:hardware_engineer) { career_tags(:hardware_engineer) }
 
   before do
-
-    # Tag association
-    prp.tags << programming
-    pra.tags << programming
-    fc1.tags << maths
-    fc2.tags << maths
-    iai.tags << robotics
-    dst.tags << algorithms
-    fc2.tags << algorithms
-    iai.tags << artificial_intelligence
-    prp.tags << software_engineer
-    pra.tags << software_engineer
-    ins.tags << network_engineer
-    ela.tags << logic_engineer
-    dbs.tags << database_engineer
-    cs1.tags << hardware_engineer
-    pra.tags << front_end_developer
-
-    nms.departments << informatics
-
-    computer_science_15.year_structures << cs_year_1
-    computer_science_15.year_structures << cs_year_2
-
-    # Group-Modules association
-    cs1_semester_1.uni_modules << prp
-    cs1_semester_1.uni_modules << ela
-    cs1_semester_1.uni_modules << fc1
-    cs1_semester_1.uni_modules << cs1
-    cs1_semester_2.uni_modules << pra
-    cs1_semester_2.uni_modules << dst
-    cs1_semester_2.uni_modules << dbs
-    cs1_semester_2.uni_modules << iai
-    cs2_semester_1.uni_modules << ins
-    cs2_semester_2.uni_modules << fc2
-
-    # Group-YearStructure association
-    cs_year_1.groups << cs1_semester_1
-    cs_year_1.groups << cs1_semester_2
-    cs_year_2.groups << cs2_semester_1
-    cs_year_2.groups << cs2_semester_2
-
-    informatics.courses << computer_science_15
+    setup_database_associations
   end
-
 
   scenario "can select and save their modules" do
     visit login_path
