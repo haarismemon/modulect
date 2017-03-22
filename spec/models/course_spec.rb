423c8d22ff077d95d471c8a5d47145f46de33e1f
k1502691 <vlad.nedelescu@kcl.ac.uk>
2017-03-22 15:38:10 +0000

    Fix test suite

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -5,8 +5,6 @@ RSpec.describe Course, type: :model do
   let (:course) { build(:course) }
 
   describe "#valid?" do
-    binding.pry
-
     context "when built by FactoryGirl" do
       it "evaluates to true" do
         expect(course.valid?).to eq true
