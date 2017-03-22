336aa62391c919c33b1889311d566471fa22978d
k1502691 <vlad.nedelescu@kcl.ac.uk>
2017-03-22 21:02:28 +0000

    Check for valid associations in UniModule upload

--- app/helpers/admin/upload_helper.rb
+++ app/helpers/admin/upload_helper.rb
@@ -9,46 +9,45 @@ module Admin::UploadHelper
 
   def upload_uni_module(new_record)
     # Lookup if Module already exists: Module is unique by Code
-    created_module = UniModule.find_by_code(new_record['code'])
+    csv_module = UniModule.find_by_code(new_record['code'])
 
     creations = 0
     updates = 0
 
-    if created_module.nil?
-      create_new_module(new_record)
+    if csv_module.nil?
+      csv_module = try_to_create_module(new_record)
       creations += 1
     else
-      update_existing_module(new_record)
+      csv_module = try_to_update_module(new_record)
       updates += 1
     end
+
+    should_save = check_module_errors(csv_module)
+
+    if should_save
+      csv_module.save
+    end
+
     return creations, updates
   end
 
   private
-  def create_new_module(new_record)
-    created_module = UniModule.create(new_record.except(
+  def try_to_create_module(new_record)
+    created_module = UniModule.new(new_record.except(
         'departments',
         'prerequisite_modules',
         'career_tags',
         'interest_tags'))
 
-    if created_module.new_record?
-      creation_error = String.new
-      created_module.errors.full_messages.each do |message|
-        creation_error += message + '; '
-      end
-      flash[:error] = creation_error
-      return
-    end
-
     update_departments(created_module, new_record)
     update_prerequisite_modules(created_module, new_record)
     update_career_tags(created_module, new_record)
     update_interest_tags(created_module, new_record)
+
+    created_module
   end
 
-  def update_existing_module(new_record)
-
+  def try_to_update_module(new_record)
     updated_module = UniModule.find_by_code(new_record['code'])
 
     updated_module.assign_attributes(new_record.except(
@@ -57,59 +56,16 @@ module Admin::UploadHelper
                   'career_tags',
                   'interest_tags'))
 
-    unless updated_module.valid?
-      update_error = "Update Failed: "
-      debugger
-      updated_module.errors.full_messages.each do |message|
-        update_error += message + '; '
-      end
-      flash[:error] = update_error
-      return
-    end
-
     update_departments(updated_module, new_record)
     update_prerequisite_modules(updated_module, new_record)
     update_career_tags(updated_module, new_record)
     update_interest_tags(updated_module, new_record)
-  end
 
-  def update_prerequisite_modules(created_module, new_record)
-    created_module.uni_modules.clear unless created_module.uni_modules.blank?
-    uploaded_prerequisites = new_record['prerequisite_modules']
-    unless uploaded_prerequisites.nil?
-      uploaded_prerequisites = uploaded_prerequisites.split(';')
-      if !uploaded_prerequisites.include? created_module.name
-        uploaded_prerequisites.each do |mod|
-          chosen_mod = UniModule.find_by_name(mod)
-          created_module.uni_modules << chosen_mod
-        end
-      else
-        created_module.errors[:base] << 'A module cannot be a prerequisite of itself.'
-      end
-    end
-  end
-
-  def update_career_tags(created_module, new_record)
-    created_module.tags.clear unless created_module.tags.blank?
-    uploaded_career_tags = new_record['career_tags']
-    unless uploaded_career_tags.nil?
-      uploaded_career_tags = uploaded_career_tags.split(';')
-      uploaded_career_tags.each do |tag|
-        chosen_tag = Tag.find_by_name(tag)
-        # Add the career tag association
-        if chosen_tag.present?
-          created_module.tags << chosen_tag
-        else
-          # If tag does not already exist then create a new tag
-          new_tag = Tag.new(name: tag, type: 'CareerTag')
-          created_module.tags << new_tag
-        end
-      end
-    end
+    updated_module
   end
 
   def update_departments(created_module, new_record)
-    created_module.departments.clear unless created_module.departments.blank?
+    created_module.departments.clear
     uploaded_departments = new_record['departments']
     unless uploaded_departments.nil?
       uploaded_departments = uploaded_departments.split(';')
@@ -120,29 +76,93 @@ module Admin::UploadHelper
             chosen_dept = Department.find_by_name(dept)
             created_module.departments << chosen_dept
           end
-        else
-          created_module.errors[:base] << "Module must also belong to your department (#{user_dept})."
         end
       end
     end
   end
 
-  def update_interest_tags(created_module, new_record)
-    created_module.interest_tags.clear unless created_module.interest_tags.blank?
-    uploaded_interest_tags = new_record['interest_tags']
-    unless uploaded_interest_tags.nil?
-      uploaded_interest_tags = uploaded_interest_tags.split(',')
-      uploaded_interest_tags.each do |tag|
+  def update_prerequisite_modules(created_module, new_record)
+    created_module.uni_modules.clear
+    uploaded_prerequisites = new_record['prerequisite_modules']
+    unless uploaded_prerequisites.nil?
+      uploaded_prerequisites = uploaded_prerequisites.split(';')
+      if !uploaded_prerequisites.include? created_module.name
+        uploaded_prerequisites.each do |mod|
+          chosen_mod = UniModule.find_by_name(mod)
+          created_module.uni_modules << chosen_mod
+        end
+      end
+    end
+  end
+
+  def update_career_tags(created_module, new_record)
+    created_module.tags.clear
+    uploaded_career_tags = new_record['career_tags']
+    unless uploaded_career_tags.blank?
+      uploaded_career_tags = uploaded_career_tags.split(';')
+      uploaded_career_tags.each do |tag|
         chosen_tag = Tag.find_by_name(tag)
-        # Add the interest tag association
         if chosen_tag.present?
           created_module.tags << chosen_tag
         else
-          # If tag does not already exist then create a new tag
+          new_tag = Tag.new(name: tag, type: 'CareerTag')
+          created_module.tags << new_tag
+        end
+      end
+    end
+  end
+
+
+  def update_interest_tags(created_module, new_record)
+    created_module.interest_tags.clear
+    uploaded_interest_tags = new_record['interest_tags']
+    unless uploaded_interest_tags.blank?
+      uploaded_interest_tags = uploaded_interest_tags.split(',')
+      uploaded_interest_tags.each do |tag|
+        chosen_tag = Tag.find_by_name(tag)
+        if chosen_tag.present?
+          created_module.tags << chosen_tag
+        else
           new_tag = Tag.new(name: tag, type: 'InterestTag')
           created_module.tags << new_tag
         end
       end
     end
+  end
+
+  def check_module_errors(csv_module)
+    valid_module = false
+    if valid_base_attributes?(csv_module) && valid_associations(csv_module)
+      valid_module = true
+    else
+      display_errors csv_module
+    end
+
+    valid_module
+  end
+
+  def valid_base_attributes?(record)
+    record.valid?
+  end
+
+  def valid_associations(csv_module)
+    valid_module = true
+    if csv_module.tags.empty?
+      csv_module.errors[:base] << "At least a Tag must be given"
+      valid_module = false
+    elsif csv_module.departments.empty?
+      csv_module.errors[:base] << "At least a Department must be given"
+      valid_module = false
+    end
+
+    valid_module
+  end
+
+  def display_errors(csv_module)
+    update_error = "Update Failed: "
+    csv_module.errors.full_messages.each do |error|
+      update_error += error + '; '
+    end
+    flash[:error] = update_error
   end
 end
