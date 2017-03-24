579d858314c490921237ccef76288a81fe59bd6e
Khalil Ahmed <mohamad.ahmed@kcl.ac.uk>
2017-03-24 15:25:20 +0000

    Refactor upload courses helper

--- app/helpers/admin/upload_courses_helper.rb
+++ app/helpers/admin/upload_courses_helper.rb
@@ -8,16 +8,20 @@ module Admin::UploadCoursesHelper
 
     csv_course = find_course_from_database(new_record)
 
+    # If departments cell is left empty in csv then turn it into empty string
+    # So that nothing is called on nil
     new_record_departments = new_record['departments']
     if new_record_departments.nil?
       new_record_departments = ''
     end
 
-    if current_user.user_level != 'super_admin_access' && !being_updated?(csv_course) && !new_record_departments.include?(current_user.department.name)
+    # Department admin restrictions
+    if dept_admin_invalid_create(csv_course, new_record_departments)
       flash[:error] = "Failed to create course #{new_record['name']}, #{new_record['year']}: Course not linked to your department"
-    elsif current_user.user_level != 'super_admin_access' && being_updated?(csv_course) && (!csv_course.departments.include?(current_user.department) || !new_record_departments.include?(current_user.department.name))
+    elsif dept_admin_invalid_update(csv_course, new_record_departments)
       flash[:error] = "Failed to update course #{new_record['name']}, #{new_record['year']}: Course not linked to your department"
     else
+      # All validation checks passed
       if csv_course.blank?
         csv_course = try_to_create_course(new_record)
         creations += 1
@@ -38,6 +42,16 @@ module Admin::UploadCoursesHelper
     return creations, updates
   end
 
+  def dept_admin_invalid_update(csv_course, new_record_departments)
+    # Prevent updating courses not in their department and prevent un-linking their own dept from course
+    is_not_super_admin && being_updated?(csv_course) && (!csv_course.departments.include?(current_user.department) || !new_record_departments.include?(current_user.department.name))
+  end
+
+  def dept_admin_invalid_create(csv_course, new_record_departments)
+    # Prevent creating courses that don't belong to their department
+    is_not_super_admin && !being_updated?(csv_course) && !new_record_departments.include?(current_user.department.name)
+  end
+
   private
   def try_to_create_course(new_record)
     new_course = Course.new(new_record.except('departments'))
@@ -52,6 +66,7 @@ module Admin::UploadCoursesHelper
   end
 
   def update_departments(csv_course, new_record)
+    # Override departments
     csv_course.departments.clear
     uploaded_departments = split_multi_association_field(new_record['departments'])
     uploaded_departments.each do |dept_name|
@@ -73,7 +88,7 @@ module Admin::UploadCoursesHelper
   end
 
   def display_errors(csv_course)
-    update_error = "Update Failed: "
+    update_error = 'Update Failed: '
     csv_course.errors.full_messages.each do |error|
       update_error += error + '; '
     end
@@ -94,5 +109,9 @@ module Admin::UploadCoursesHelper
 
   def being_updated?(the_course)
     !the_course.nil?
+  end
+
+  def is_not_super_admin
+    current_user.user_level != 'super_admin_access'
   end
 end
