67054260e5e311b88cabeb5b4da85a27ef83aff5
k1502691 <vlad.nedelescu@kcl.ac.uk>
2017-03-26 15:55:38 +0100

    Test UniModule.to_csv

--- spec/models/uni_module_spec.rb
+++ spec/models/uni_module_spec.rb
@@ -81,4 +81,56 @@ RSpec.describe UniModule, type: :model do
       end
     end
   end
+
+  describe ".to_csv" do
+    let! (:course) { create(:course) }
+    let! (:department) { create(:department) }
+    let!(:career_tag) { create(:career_tag) }
+    let!(:interest_tag) { create(:interest_tag) }
+    let! (:module1) { create(:uni_module) }
+    let! (:module2) { create(:uni_module, 
+                              name: "Philosophy and Geography",
+                              code: "4LAB1PHG",
+                              semester: "2",
+                              credits: 15,
+                              )}
+    let (:csv_header) { "name,code,description,lecturers,pass_rate,assessment_methods,semester,credits,exam_percentage,coursework_percentage,more_info_link,assessment_dates,prerequisite_modules,career_tags,interest_tags,departments\n" }
+    let (:csv_content) { UniModule.to_csv(UniModule.all) }
+
+    before do
+      module2.uni_modules << module1
+      module2.career_tags << career_tag
+      module2.interest_tags << interest_tag
+    end
+   
+    it "outputs all saved modules" do
+      expect(csv_content).to include csv_header
+      test_csv_attributes_for_all_uni_modules
+    end
+  end
+
+  private
+  def test_csv_attributes_for_all_uni_modules
+    uni_modules = UniModule.all
+    csv_content.slice!(csv_header)
+    i = 0
+    CSV.parse(csv_content).each do |line|
+      line = line.join(",")
+      uni_module = uni_modules[i]
+      expect(line).to include uni_module.name
+      expect(line).to include uni_module.code
+      expect(line).to include uni_module.semester
+      expect(line).to include uni_module.credits.to_s
+      uni_module.uni_modules.each do |prerequisite_module|
+        expect(line).to include prerequisite_module.code
+      end
+      uni_module.career_tags.each do |career_tag|
+        expect(line).to include career_tag.name
+      end
+      uni_module.interest_tags.each do |interest_tag|
+        expect(line).to include interest_tag.name
+      end
+      i += 1
+    end
+  end
 end
